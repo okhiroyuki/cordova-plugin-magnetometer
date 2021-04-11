4bd7b5232d4852abbbeeba0f27cdd4e369771aaa
okhiroyuki <ok.okada.hiroyuki@gmail.com>
2021-04-11 15:13:50 +0900

    fix: add accuracy params

--- www/MagneticField.js
+++ www/MagneticField.js
@@ -1,7 +1,8 @@
-var MagneticField = function(x, y, z, magnitude, timestamp) {
+var MagneticField = function(x, y, z, accuracy, magnitude, timestamp) {
   this.x = x;
   this.y = y;
   this.z = z;
+  this.accuracy = accuracy;
   this.magnitude = magnitude;
   this.timestamp = timestamp || (new Date()).getTime();
 };
